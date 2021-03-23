d1b45d098c6e4e00cbf1d6c53bc1b375118813d6
RyanSyme <ryu321@hotmail.com>
2021-03-23 11:45:43 +0000

    add stripe keys and functionality

--- house_of_sock/settings.py
+++ house_of_sock/settings.py
@@ -168,3 +168,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 30
+
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
