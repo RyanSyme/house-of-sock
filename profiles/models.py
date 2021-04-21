45b038d74d465412582ababf444191a79da6063a
RyanSyme <ryu321@hotmail.com>
2021-04-21 09:58:14 +0000

    add profile app and template

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class UserProfile(models.Model):
+    """
+    Profile model to save order history and personal info
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_country = CountryField(blank_label="Country *", null=True, blank=False)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=False)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=False)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Creates and/or updates the users profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Saves the profile for existing users
+    instance.userprofile.save()
