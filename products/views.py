3cd0dd3b726e40de0ea41baf3fdda99436ccedd5
RyanSyme <ryu321@hotmail.com>
2021-04-26 12:14:52 +0000

    add image functionality to add_product url

--- products/views.py
+++ products/views.py
@@ -57,7 +57,17 @@ def add_product(request):
     """
     Give admin ability to add products to the store
     """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added a product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed tp add product. Please check to ensure the form is valid')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
