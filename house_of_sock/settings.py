0871e44528c8f3959c06c284b17a73fe22a12902
RyanSyme <ryu321@hotmail.com>
2021-04-28 08:24:07 +0000

    add secret key for testing

--- house_of_sock/settings.py
+++ house_of_sock/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'ivtf@6-(i3%)gy0jhew^a!844)w+zqijyzj@m&t$tcwc_tano%')
 
-DISABLE_COLLECTSTATIC = 1
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
