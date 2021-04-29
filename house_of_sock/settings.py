ce2dcda871a34eafe4c1da4f35a0935bab5d4fe1
RyanSyme <ryu321@hotmail.com>
2021-04-29 13:27:44 +0000

    remove SECRET_KEY and set debug

--- house_of_sock/settings.py
+++ house_of_sock/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%*zarr06xipf)ze+e1ncn%i@9t3vef+6t&cwuy-4vv(qb(%zk('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['house-of-sock.herokuapp.com', 'localhost']
 
