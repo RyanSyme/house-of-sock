8fed01c32cc87765d667b1711499b546fc464fe0
RyanSyme <ryu321@hotmail.com>
2021-05-03 12:36:57 +0000

    add review.html and review form to product detail page

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 
 class Category(models.Model):
@@ -29,3 +31,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50, blank=True)
+    comment = models.CharField(max_length=250, blank=True)
+    rating = models.IntegerField(default=1)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['subject', 'comment', 'rating']
