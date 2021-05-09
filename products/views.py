2543f2afcbdaee729abfc130bb2afceeb1cefda9
RyanSyme <ryu321@hotmail.com>
2021-05-09 14:47:34 +0000

    styling for product_detail.html

--- products/views.py
+++ products/views.py
@@ -61,7 +61,7 @@ def product_detail(request, product_id):
         'product': product
     }
 
-    return render(request, 'products/product_detail.html', context, review)
+    return render(request, 'products/product_detail.html', context)
 
 
 @login_required
