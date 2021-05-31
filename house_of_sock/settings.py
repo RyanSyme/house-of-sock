3a1a0db8a574ee3ea7c41512f00778a9e69691de
RyanSyme <ryu321@hotmail.com>
2021-05-31 12:48:58 +0000

    change debug to false

--- house_of_sock/settings.py
+++ house_of_sock/settings.py
@@ -28,7 +28,8 @@ if os.path.exists("env.py"):
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['house-of-sock.herokuapp.com', 'localhost']
 
@@ -127,13 +128,13 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
