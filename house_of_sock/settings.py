bff6d106221b471f4a6b67f55a941a833df8732b
RyanSyme <72189430+RyanSyme@users.noreply.github.com>
2022-11-24 12:47:23 +0100

    Update settings.py

--- house_of_sock/settings.py
+++ house_of_sock/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['house-of-sock.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['house-of-sock.onrender.com', 'localhost']
 
 
 # Application definition
